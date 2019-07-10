bbfba46315be3cd72503968070c1761368e21811
huisedediao <401758785@qq.com>
2019-07-10 18:40:39 +0800

    base commit

---
+++ XBCategoryDemo/XBCategoryDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XBCategoryDemo
+//
+//  Created by xxb on 2019/7/10.
+//  Copyright © 2019 xxb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XBCategorys/NSString+MD5.h
@@ -0,0 +1,24 @@
+//
+//  NSString+MD5.h
+//  TongMeng
+//
+//  Created by 谢贤彬 on 2019/2/16.
+//  Copyright © 2019年 xxb. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSString (MD5)
+/** 32位小写 */
+- (NSString *)md5HashToLower32Bit;
+/** 32位大写 */
+- (NSString *)md5HashToUpper32Bit;
+/** 16位小写 */
+- (NSString *)md5HashToLower16Bit;
+/** 16位大写 */
+- (NSString *)md5HashToUpper16Bit;
+@end
+
+NS_ASSUME_NONNULL_END
